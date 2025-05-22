b6f92d474b411c18384b81feba78a7728df2e997
BastiGarrido <bas.garrido@duocuc.cl>
2025-05-22 18:40:11 -0400

    Versión inicial del proyecto Ferretería con Django

---
+++ pagina/static/js/animaciones.js
@@ -0,0 +1,4 @@
+// Este archivo está disponible para futuras funciones JS personalizadas
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Página de inicio cargada correctamente.");
+});
